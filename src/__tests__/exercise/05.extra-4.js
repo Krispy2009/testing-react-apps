bc380424c2fd115328dcd654e834c5fd5d40d3dd
Kristi Achilleos <krispy2009@gmail.com>
2021-11-07 14:29:56 +0200

    finish ex5 and extras

---
+++ src/__tests__/exercise/05.extra-4.js
@@ -0,0 +1,70 @@
+// mocking HTTP requests
+// http://localhost:3000/login-submission
+
+import * as React from 'react'
+import {rest} from 'msw'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {build, fake} from '@jackfranklin/test-data-bot'
+import {setupServer} from 'msw/node'
+import Login from '../../components/login-submission'
+import {handlers} from '../../test/server-handlers'
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
+
+const server = setupServer(...handlers)
+
+beforeAll(() => server.listen())
+beforeEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test(`logging in displays the user's username`, async () => {
+  render(<Login />)
+  const {username, password} = buildLoginForm()
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/))
+
+  expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+test(`no password shows alert`, async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/))
+  expect(screen.getByRole('alert')).toMatchInlineSnapshot(`
+    <div
+      role="alert"
+      style="color: red;"
+    >
+      password required
+    </div>
+  `)
+})
+
+test('server 500 handled gracefully', async () => {
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: 'OH NO'}))
+      },
+    ),
+  )
+  render(<Login />)
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/))
+  expect(screen.getByRole('alert')).toHaveTextContent('OH NO')
+})
