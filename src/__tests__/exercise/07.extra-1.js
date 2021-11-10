72540a4aefcc8871bb65f67ad2e1dd701b23a0a1
Kristi Achilleos <krispy2009@gmail.com>
2021-11-10 20:30:35 +0200

    finish ex7 and extras

--- src/__tests__/exercise/07.extra-1.js
+++ src/__tests__/exercise/07.extra-1.js
@@ -9,10 +9,24 @@ import EasyButton from '../../components/easy-button'
 test('renders with the light styles for the light theme', () => {
   const Wrapper = ({children}) => <ThemeProvider>{children}</ThemeProvider>
   render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
-    background-color: white;
-    color: black;
+  background-color: white;
+  color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  )
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
   `)
 })
 
